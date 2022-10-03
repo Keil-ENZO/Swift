9017119ef43224b42b52b591ff4dad54e152247f
Keil-ENZO <104256342+Keil-ENZO@users.noreply.github.com>
2022-10-03 13:48:12 +0200

    Initial Commit

---
+++ wallet/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  wallet
+//
+//  Created by Enzo Keil on 03/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wallet/walletApp.swift
@@ -0,0 +1,17 @@
+//
+//  walletApp.swift
+//  wallet
+//
+//  Created by Enzo Keil on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct walletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
